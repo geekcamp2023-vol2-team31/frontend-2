b6b26fc3b5493ed708257cf6663d8680bd65a712
kitoken99 <kitoken99@gmail.com>
2023-05-27 14:40:08 +0900

    線の描画作成/再レンダリング時の挙動要確認

--- src/components/teams/InterelementLink.tsx
+++ src/components/teams/InterelementLink.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from "react";
-
+import React, { FC, useEffect, useRef } from "react";
+// const width= 100000
+// const height = 100000
 interface IInterelementLink {
   leftItemId: string;
   rightItemId: string;
@@ -10,8 +11,38 @@ interface IInterelementLink {
   emphasized?: true;
 }
 
-const InterelementLink: FC<IInterelementLink> = (/* props */) => {
-  return <></>;
+const InterelementLink: FC<IInterelementLink> = (props) => {
+  const { /*leftItemId, rightItemId,*/ x0, y0, x1, y1 /*, emphasized*/ } =
+    props;
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    if (!canvasRef.current) {
+      throw new Error("objectがnull");
+    }
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+      throw new Error("context取得失敗");
+    }
+
+    ctx.strokeStyle = "#black";
+    ctx.beginPath();
+    ctx.moveTo(x0, y0);
+    ctx.lineTo(x1, y1);
+    ctx.stroke();
+  }, []);
+
+  return (
+    <div>
+      <canvas
+        ref={canvasRef}
+        width={10000}
+        height={10000}
+        style={{ position: "absolute" }}
+      />
+    </div>
+  );
 };
 
 export default InterelementLink;
