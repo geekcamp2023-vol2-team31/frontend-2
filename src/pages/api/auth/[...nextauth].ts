a5c37e4bcdc48c15db9799c25f64d0d1e9b49774
kitoken99 <kitoken99@gmail.com>
2023-06-11 22:29:18 +0900

    auth認証の実装

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,27 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-export const authOptions = {
+
+export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
 export default NextAuth(authOptions);
