a2946af167602a10bbf39aaaeaaf367405b7b0f6
kitoken99 <kitoken99@gmail.com>
2023-06-11 22:39:30 +0900

    不要ファイルの削除

--- src/components/teams/InterelementLink.tsx
+++ src/components/teams/InterelementLink.tsx
@@ -1,55 +1,17 @@
-import React, { FC, useEffect, useRef } from "react";
-import styles from "./InterelementLink.module.css";
+import React, { FC } from "react";
 
 interface IInterelementLink {
-  // leftItemId: string;
-  // rightItemId: string;
+  leftItemId: string;
+  rightItemId: string;
   x0: number;
   y0: number;
   x1: number;
   y1: number;
-  emphasized: boolean;
+  emphasized?: true;
 }
 
-const InterelementLink: FC<IInterelementLink> = (props) => {
-  const { /*leftItemId, rightItemId,*/ x0, y0, x1, y1, emphasized } = props;
-  const margin = 10;
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  useEffect(() => {
-    if (!canvasRef.current) {
-      throw new Error("objectがnull");
-    }
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    if (!ctx) {
-      throw new Error("context取得失敗");
-    }
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    emphasized
-      ? (ctx.strokeStyle = "rgba(255,0,0,1)")
-      : (ctx.strokeStyle = "rgba(255,0,0,0.3)");
-    emphasized ? (ctx.lineWidth = 5) : (ctx.lineWidth = 4);
-    ctx.lineCap = "round";
-    ctx.beginPath();
-    ctx.moveTo(margin, Math.max(y0 - y1 + margin, margin));
-    ctx.lineTo(x1 - x0 + margin, Math.max(y1 - y0 + margin, margin));
-    ctx.stroke();
-  }, [x0, y0, x1, y1, emphasized]);
-
-  return (
-    <div>
-      <canvas
-        ref={canvasRef}
-        width={x1 - x0 + 2 * margin}
-        height={Math.abs(y1 - y0) + 2 * margin}
-        className={styles.link}
-        style={{
-          left: `${x0 - margin}px`,
-          top: `${Math.min(y1, y0) - margin}px`,
-        }}
-      />
-    </div>
-  );
+const InterelementLink: FC<IInterelementLink> = (/* props */) => {
+  return <></>;
 };
 
 export default InterelementLink;
