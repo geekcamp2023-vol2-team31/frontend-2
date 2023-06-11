a6bd6980739e8671af9f6e3c53a3a7ddd507faf3
Yajima Soichi <bildeneins@gmail.com>
2023-06-11 23:03:00 +0900

    Format

--- src/components/teams/IdeaList.tsx
+++ src/components/teams/IdeaList.tsx
@@ -1,13 +1,13 @@
-import React, { FC, useEffect, useState } from 'react';
-import IdeaListItem from './IdeaListItem';
-import classes from './IdeaList.module.css';
-import { ReactSortable } from 'react-sortablejs';
+import React, { FC, useEffect, useState } from "react";
+import IdeaListItem from "./IdeaListItem";
+import classes from "./IdeaList.module.css";
+import { ReactSortable } from "react-sortablejs";
 
 interface IIdeaListProps {
   id: string;
   label: string;
-  leftStyle: 'circle' | 'triangle'; // 子要素の全てのIdeaListItemのleftStyle
-  rightStyle: 'circle' | 'triangle'; // 子要素の全てのIdeaListItemのrightStyle
+  leftStyle: "circle" | "triangle"; // 子要素の全てのIdeaListItemのleftStyle
+  rightStyle: "circle" | "triangle"; // 子要素の全てのIdeaListItemのrightStyle
   items: {
     id: string;
     value: string;
@@ -45,23 +45,33 @@ interface IItemHeight {
 const offsetYOfFirstItem = 14 + 24 + 12;
 
 // IdeaListItem間のマージン 単位: px
-const itemMargin = 8; 
+const itemMargin = 8;
 
-const IdeaList: FC<IIdeaListProps> = ({ id, label, leftStyle, rightStyle, items, onChangeItemsHeight, onChangeItems }) => {
+const IdeaList: FC<IIdeaListProps> = ({
+  id,
+  label,
+  leftStyle,
+  rightStyle,
+  items,
+  onChangeItemsHeight,
+  onChangeItems,
+}) => {
   const [itemHeights, setItemHeights] = useState<IItemHeight[]>(
     // 初期値は全idにheight: 0を割り当てる。
-    items.map(item => ({ id: item.id, height: 0 }))
+    items.map((item) => ({ id: item.id, height: 0 }))
   );
 
   // 子要素の高さが変わったときにonChangeItemsHeightを呼び出す
   useEffect(() => {
     if (onChangeItemsHeight) {
       const resultItems: {
-        id: string, height: number, offsetY: number
+        id: string;
+        height: number;
+        offsetY: number;
       }[] = [];
 
       let offsetY = offsetYOfFirstItem;
-      itemHeights.forEach(item => {
+      itemHeights.forEach((item) => {
         resultItems.push({
           ...item,
           offsetY,
@@ -73,42 +83,49 @@ const IdeaList: FC<IIdeaListProps> = ({ id, label, leftStyle, rightStyle, items,
     }
   }, [id, itemHeights, onChangeItemsHeight]);
 
-  const handleChangeHeight = ({ id, height }: {id: string, height: number}) => {
-    setItemHeights(itemHeights => {
+  const handleChangeHeight = ({
+    id,
+    height,
+  }: {
+    id: string;
+    height: number;
+  }) => {
+    setItemHeights((itemHeights) => {
       // 該当のitemの高さを更新する
-      const nextItemHeights = itemHeights.map(item => {
-        if (item.id === id) { return { id, height }}
-        else { return item }
+      const nextItemHeights = itemHeights.map((item) => {
+        if (item.id === id) {
+          return { id, height };
+        } else {
+          return item;
+        }
       });
       return nextItemHeights;
     });
-  }
+  };
 
-  const handleSetItems = (items: { id: string, value: string }[]) => {
+  const handleSetItems = (items: { id: string; value: string }[]) => {
     if (onChangeItems) {
       onChangeItems({ id, items });
     }
-  }
+  };
 
   return (
     <section className={classes.card}>
       <h2 className={classes.heading}>{label}</h2>
       <ReactSortable
-        className={classes['children-container']}
+        className={classes["children-container"]}
         list={items}
         setList={handleSetItems}
       >
-        {
-          items.map(item => (
-            <IdeaListItem
-              key={item.id}
-              {...item}
-              leftStyle={leftStyle}
-              rightStyle={rightStyle}
-              onChangeHeight={handleChangeHeight}
-            />
-          ))
-        }
+        {items.map((item) => (
+          <IdeaListItem
+            key={item.id}
+            {...item}
+            leftStyle={leftStyle}
+            rightStyle={rightStyle}
+            onChangeHeight={handleChangeHeight}
+          />
+        ))}
       </ReactSortable>
     </section>
   );
