8ca0ba4f718aa98c5c2f5629a633c8dddf192b86
satojin219 <renzuoteng142@gmail.com>
2023-06-17 21:55:06 +0900

    feat: create chipWithButtons

--- src/components/teams/ChipWithButtons/ChipWithButtons.stories.tsx
+++ src/components/teams/ChipWithButtons/ChipWithButtons.stories.tsx
@@ -1,5 +1,11 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { ChipWithButtons } from "./ChipWithButtons.presenter";
+import { ChipButton } from "../ChipButton/ChipButton";
+import { FlagIcon } from "@/assets/flagIcon";
+import style from "./ChipWithButtons.module.scss";
+import { DrawIcon } from "@/assets/drawIcon";
+import { SchoolIcon } from "@/assets/schoolIcon";
+import { DeleteIcon } from "@/assets/deleteIcon";
 
 type Story = StoryObj<typeof ChipWithButtons>;
 
@@ -10,4 +16,44 @@ const meta = {
 
 export default meta;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    id: "React",
+    label: "React",
+    leftSlot: <div></div>,
+    buttonGroup: (
+      <>
+        <ChipButton
+          target={{ key: "beginner", level: "beginner" }}
+          className={style.buttonReset}
+          onClick={() => console.log("clicked!")}
+        >
+          <FlagIcon className={style.button} />
+        </ChipButton>
+        <ChipButton
+          target={{ key: "advanced", level: "advanced" }}
+          className={style.buttonReset}
+          onClick={() => console.log("clicked!")}
+        >
+          <DrawIcon className={style.button} />
+        </ChipButton>
+        <ChipButton
+          target={{ key: "expert", level: "expert" }}
+          className={style.buttonReset}
+          onClick={() => console.log("clicked!")}
+        >
+          <SchoolIcon className={style.active} />
+        </ChipButton>
+        <ChipButton
+          target={{ key: "delete", level: "delete" }}
+          className={style.buttonReset}
+          onClick={() => console.log("clicked!")}
+        >
+          <DeleteIcon className={style.active} />
+        </ChipButton>
+      </>
+    ),
+    background: "#3998B6",
+  },
+  render: (args) => <ChipWithButtons {...args} />,
+};
