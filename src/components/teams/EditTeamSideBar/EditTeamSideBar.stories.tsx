c059e6425b0e44caece57f5055ffd69cf1723a28
satojin219 <renzuoteng142@gmail.com>
2023-06-19 14:49:11 +0900

    feat: create EditTeamSideBar

---
+++ src/components/teams/EditTeamSideBar/EditTeamSideBar.stories.tsx
@@ -0,0 +1,52 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { EditTeamSideBar } from "./EditTeamSideBar";
+
+const meta: Meta<typeof EditTeamSideBar> = {
+  component: EditTeamSideBar,
+};
+export default meta;
+
+export const Default: StoryObj<typeof EditTeamSideBar> = {};
+Default.args = {
+  ideaName: "技術COMP",
+  estimatedTechList: [
+    {
+      id: "1",
+      label: "TypeScript",
+      users: [
+        { id: "1", name: "user1", level: "beginner" },
+        { id: "2", name: "user2", level: "advanced" },
+        { id: "3", name: "user3", level: "advanced" },
+      ],
+      actionType: "plus",
+      onActionClick: () => console.log("click"),
+      leftSlot: <></>,
+    },
+    {
+      id: "2",
+      label: "Node.js",
+      users: [
+        { id: "1", name: "user1", level: "beginner" },
+        { id: "3", name: "user3", level: "expert" },
+      ],
+      actionType: "plus",
+      onActionClick: () => console.log("click"),
+      leftSlot: <></>,
+    },
+  ],
+  memberHasTechList: [
+    {
+      id: "1",
+      label: "TypeScript",
+      users: [
+        { id: "1", name: "user1", level: "beginner" },
+        { id: "2", name: "user2", level: "advanced" },
+        { id: "3", name: "user3", level: "advanced" },
+      ],
+      actionType: "plus",
+      onActionClick: () => console.log("click"),
+      leftSlot: <></>,
+    },
+  ],
+  onClose: () => console.log("close"),
+};
---
+++ src/components/teams/EditTeamSideBar/EditTeamSideBar.tsx
@@ -0,0 +1,64 @@
+import { FC } from "react";
+import { ITechListItem } from "../TechListItem";
+import style from "./EditTeamSideBar.module.scss";
+import { CloseIcon } from "@/assets/close";
+import TechListItem from "../TechListItem/TechListItem";
+
+type Props = {
+  ideaName: string;
+  estimatedTechList: ITechListItem[];
+  memberHasTechList: ITechListItem[];
+  onClose: () => void;
+};
+
+export const EditTeamSideBar: FC<Props> = ({
+  ideaName,
+  estimatedTechList,
+  memberHasTechList,
+  onClose,
+}) => {
+  return (
+    <div className={style.container}>
+      <div className={style.inputContainer}>
+        <input type="text" defaultValue={ideaName} className={style.input} />
+        <button className={style.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </button>
+      </div>
+      <div className={style.techContainer}>
+        <p className={style.title}>使用予定の技術</p>
+        {estimatedTechList.map((iTech) => {
+          return (
+            <TechListItem
+              key={iTech.id}
+              id={iTech.id}
+              label={iTech.label}
+              leftSlot={iTech.leftSlot}
+              users={iTech.users}
+              actionType={iTech.actionType}
+              onActionClick={iTech.onActionClick}
+            />
+          );
+        })}
+      </div>
+      <div className={style.techContainer}>
+        <p className={style.title}>メンバーが持っている技術</p>
+        {memberHasTechList.map((iTech) => {
+          return (
+            <TechListItem
+              key={iTech.id}
+              id={iTech.id}
+              label={iTech.label}
+              leftSlot={iTech.leftSlot}
+              users={iTech.users}
+              actionType={iTech.actionType}
+              onActionClick={iTech.onActionClick}
+            />
+          );
+        })}
+      </div>
+
+      <button className={style.deleteButton}>技術COMPを削除</button>
+    </div>
+  );
+};
