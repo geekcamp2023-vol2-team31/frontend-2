f6d8773c86eb3b394ed824ef0b256b33a7622d1c
Yajima Soichi <bildeneins@gmail.com>
2023-06-21 21:47:59 +0900

    Actionを消せるようにした

--- src/components/teams/TechListItem/TechListItem.stories.tsx
+++ src/components/teams/TechListItem/TechListItem.stories.tsx
@@ -26,3 +26,14 @@ Default.args = {
   ],
   actionType: "plus",
 };
+
+export const WithoutAction: StoryObj<typeof TechListItem> = {};
+WithoutAction.args = {
+  id: "1",
+  label: "TypeScript",
+  users: [
+    { id: "1", name: "user1", level: "beginner" },
+    { id: "2", name: "user2", level: "advanced" },
+    { id: "3", name: "user3", level: "advanced" },
+  ],
+};
