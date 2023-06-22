dfbbc1b5c90b6218df385be278c6eea7717bbdde
satojin219 <renzuoteng142@gmail.com>
2023-06-22 16:18:21 +0900

    chore: install react-tostify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,22 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session as Session}>
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
