b1ad4bf8a57f0d4450bd18ff6be330653075894f
satojin219 <renzuoteng142@gmail.com>
2023-06-22 17:28:02 +0900

    feat: login and access restrictions

--- src/components/auth/LoginButton.tsx
+++ src/components/auth/LoginButton.tsx
@@ -1,17 +1,36 @@
 import React from "react";
 import style from "./LoginButton.module.css";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { requests } from "@/utils/requests";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const LoginButton = () => {
+  const { data: session } = useSession();
+  const notify = () => toast.error("ログインに失敗しました。");
+  const token = session?.user.accessToken as string;
+  const handleClick = async () => {
+    try {
+      await signIn("github");
+      await requests("/auth", {
+        body: JSON.stringify(token),
+      });
+    } catch (error) {
+      notify();
+    }
+  };
+
   return (
-    <button onClick={() => void signIn("github")} className={style.button}>
-      <img
-        src="./assets/github-icon.svg"
-        width={25}
-        height={25}
-        alt="GitHubのアイコン"
-      />
-      GitHubでログインする
-    </button>
+    <>
+      <button onClick={handleClick as () => void} className={style.button}>
+        <img
+          src="./assets/github-icon.svg"
+          width={25}
+          height={25}
+          alt="GitHubのアイコン"
+        />
+        GitHubでログインする
+      </button>
+    </>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
 import { Session } from "next-auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const queryClient = new QueryClient();
+  const router = useRouter();
+  const { data } = useSession();
+
+  useEffect(() => {
+    if (!data || session) {
+      void router.push("/");
+    }
+  }, [data, router, session]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session as Session}>
